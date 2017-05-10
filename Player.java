18e25cbc0c5fe55328d169b04158b1563969f3cf
Ben Swanson <benswanson127@gmail.com>
2017-05-10 16:57:24 -0600

    Added temp files

---
+++ Player.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+
+public class Player {
+	String name;
+	Double money;
+	
+	public Player(String n, Double m) {
+		m = money;
+		n = name;
+	}
+
+}
---
+++ World.java
@@ -0,0 +1,18 @@
+
+public class World {
+
+	int daysOld;
+	int currentDay;
+	int econStrength;
+	int season;
+	String worldName;
+	
+	public World(int d, int cd, int es, int se, String wn) {
+		d = daysOld;
+		cd = currentDay;
+		es = econStrength;
+		se = season;
+		wn = worldName;
+	}
+	
+}
